8d81ace06e1db7ed5ec77c9aabd8df6965a56d20
Lesia Humenna <lgumenna61@gmail.com>
2023-06-23 14:07:32 +0300

    add js

---
+++ 23.06.23-Hillel-12lesson/main.js
@@ -0,0 +1,13 @@
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateKey(length){
+    let key = '';
+        for(let i = 0; i < length; i++){
+            let randomChar = Math.floor(Math.random() * characters.length);
+            let stringRandom = characters.charAt(randomChar);
+            key += stringRandom;
+        }
+    return key
+}
+let key = generateKey(16);
+    console.log(key);
